9e10b0b8d424e4cee5513e4a6a296e1c406c84e8
beqasabana <bsabanashvili@gmail.com>
2022-01-29 21:40:19 -0500

    added review model

--- flask_app/models/review.py
+++ flask_app/models/review.py
@@ -2,12 +2,11 @@ from flask_app.config.mysqlconnection import connectToMySQL
 from flask_app.models.user import User
 
 
-class Landlord:
+class Review:
     def __init__(self, data):
         self.id = data['id']
-        self.user = User.get_user_by_id({'id': data['user_id']})
-        self.name = data['name']
-        self.address = data['address']
-        self.reviews = []
+        self.user = User.get_user_by_id({'id': data['id']})
+        self.landlord_id = data['lendlord_id']
+        self.text = data['text']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']

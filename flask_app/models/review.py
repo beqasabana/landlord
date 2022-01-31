67291382ae9ac35f3d96801629671603dd009a6e
beqasabana <bsabanashvili@gmail.com>
2022-01-31 12:14:01 -0500

    added review validation and save methods

--- flask_app/models/review.py
+++ flask_app/models/review.py
@@ -1,4 +1,5 @@
 from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
 from flask_app.models.user import User
 
 
@@ -10,3 +11,19 @@ class Review:
         self.text = data['text']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
+
+    # returns inserted row id or False if something goes wrong
+    @classmethod
+    def save(cls, data):
+        query = "INSERT INTO reviews (landlord_id, user_id, text) VALUES (%(landlord_id)s, %(user_id)s, %(text)s);"
+        review_id = connectToMySQL('landlord').query_db(query, data)
+        return review_id
+
+    # takes in the form and returns True or False with flashed messages
+    @staticmethod
+    def validate_review(form):
+        is_valid = True
+        if len(form['text']) <= 0:
+            is_valid = False
+            flash("Cant leave empty review.", 'review-error')
+        return is_valid

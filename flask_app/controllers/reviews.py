f937493310fafb891776f91a87e191bc2be56324
beqasabana <bsabanashvili@gmail.com>
2022-02-02 23:02:51 -0500

    code cleanup

--- flask_app/controllers/reviews.py
+++ flask_app/controllers/reviews.py
@@ -18,51 +18,16 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-
-
-@app.route('/review')
-def review():
-    return render_template('review.html')
-
-@app.route('/review', methods=['POST'])
-def upload_image():
-	if 'file' not in request.files:
-		flash('No file part')
-		return redirect('review.html')
-	file = request.files['file']
-	if file.filename == '':
-		flash('No image selected for uploading')
-		return redirect('review.html')
-	if file and allowed_file(file.filename):
-		filename = secure_filename(file.filename)
-		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-		#print('upload_image filename: ' + filename)
-		flash('Image successfully uploaded and displayed below')
-		return render_template('review.html', filename=filename)
-	else:
-		flash('Allowed image types are -> png, jpg, jpeg, gif')
-		return redirect('review.html')
-
 @app.route('/review/<filename>')
 def display_image(filename):
 	#print('display_image filename: ' + filename)
 	return redirect(url_for('static', filename='uploads/' + filename), code=301)
 
 
-
-
-
-
-
-
 @app.route('/create/review/<int:landlord_id>', methods = ['POST'])
 def create_review_for_landlord(landlord_id):
-
-
-
     if 'file' not in request.files:
         flash('No file part')
-
     file = request.files['file']
     if file.filename == '':
         flash('No image selected for uploading')
@@ -73,12 +38,6 @@ def create_review_for_landlord(landlord_id):
         flash('Image successfully uploaded and displayed below')
     else:
         flash('Allowed image types are -> png, jpg, jpeg, gif')
-
-
-
-
-
-
     active_user = User.get_user_by_id({'id': session['user']})
     try:
         review_data = {
